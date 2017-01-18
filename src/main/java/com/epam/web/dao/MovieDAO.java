3b4647c7d00b22046c012d622906b82ce756be16
mockturtleqt <galia.semashko@gmail.com>
2017-01-18 19:11:21 +0300

    Services added.

--- src/main/java/com/epam/web/dao/MovieDAO.java
+++ src/main/java/com/epam/web/dao/MovieDAO.java
@@ -2,19 +2,14 @@ package com.epam.web.dao;
 
 import com.epam.web.dbConnection.ProxyConnection;
 import com.epam.web.entity.Movie;
-import com.epam.web.entity.type.GenreType;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Array;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static com.epam.web.dbConnection.SQLQueries.SQL_SELECT_ALL_MOVIES_BY_TITLE;
@@ -23,12 +18,12 @@ import static com.epam.web.dbConnection.SQLQueries.SQL_SELECT_MOVIE_BY_TITLE;
 public class MovieDAO extends AbstractDAO<Movie> {
     private static final String ID = "id";
     private static final String TITLE = "title";
-    private static final String DURATION =  "duration";
-    private static final String RELEASE_DATE =  "release_date";
+    private static final String DURATION = "duration";
+    private static final String RELEASE_DATE = "release_date";
     private static final String DESCRIPTION = "description";
     private static final String POSTER = "poster";
-    private static final String RATING =  "rating";
-    private static final String GENRE =  "genre";
+    private static final String RATING = "rating";
+    private static final String GENRE = "genre";
     private static final Logger logger = LogManager.getLogger();
 
     public MovieDAO(ProxyConnection connection) {
